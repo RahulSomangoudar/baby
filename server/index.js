8a2ae92026c1a09e69909b8d96b5c295d71b9827
Rahul Somangoudar <141225419+RahulSomangoudar@users.noreply.github.com>
2025-01-08 16:52:31 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -4,36 +4,36 @@ const cors = require('cors')
 const RegisterModel = require('./models/Register')
 
 const app = express()
-app.use(cors(
-    {
-        origin: ["https://baby-frontend-ochre.vercel.app/"],
-        methods: ["POST", "GET"],
-        credentials: true
-    }
-));
-app.use(express.json())
 
-mongoose.connect('mongodb+srv://test:test123@cluster0.6yzt7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+// Update the origin URL: remove the trailing slash
+app.use(cors({
+    origin: ["https://baby-frontend-ochre.vercel.app"], // No trailing slash
+    methods: ["POST", "GET"],
+    credentials: true
+}));
 
+app.use(express.json());
+
+mongoose.connect('mongodb+srv://test:test123@cluster0.g4i5dey.mongodb.net/test?retryWrites=true&w=majority');
 
 app.get("/", (req, res) => {
     res.json("Hello");
-})
-app.post('/register', (req, res) => {
-    const {name, email, password} = req.body;
-    RegisterModel.findOne({email: email})
-    .then(user => {
-        if(user) {
-            res.json("Already have an account")
-        } else {
-            RegisterModel.create({name: name, email: email, password: password})
-            .then(result => res.json(result))
-            .catch(err => res.json(err))
-        }
-    }).catch(err => res.json(err))
-})
+});
 
+app.post('/register', (req, res) => {
+    const { name, email, password } = req.body;
+    RegisterModel.findOne({ email: email })
+        .then(user => {
+            if (user) {
+                res.json("Already have an account");
+            } else {
+                RegisterModel.create({ name: name, email: email, password: password })
+                    .then(result => res.json(result))
+                    .catch(err => res.json(err));
+            }
+        }).catch(err => res.json(err));
+});
 
 app.listen(3001, () => {
-    console.log("Server is Running")
-})
+    console.log("Server is Running");
+});
